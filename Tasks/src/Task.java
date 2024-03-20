22ac8578988366843a6c76c9154e7d487ec6889d
Oleksandr <oleksandr.ovcharenko@dci-students.org>
2024-03-20 13:43:05 +0100

    task14 done

--- Tasks/src/Task.java
+++ Tasks/src/Task.java
@@ -1,25 +1,8 @@
-import java.util.Arrays;
 public class Task {
     public static void main(String[] args) {
-        Integer[] array = {1, 4, 4, 7, 7, 7, 8};
-        System.out.println(Arrays.toString(noDuplicates(array)));
+        reverse("AnyStringYouWantToReverse");
     }
-    private static int[] noDuplicates(Integer[] source) {
-        int repeats = 0, index = 0;
-        for (int i = 1; i < source.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (source[j] != null && source[j] == source[i]) {
-                    repeats++;
-                    source[j] = null;
-                    break;
-                }
-            }
-        }
-        int[] result = new int[source.length - repeats];
-        for (int i = 0; i < source.length; i++) {
-            if (source[i] == null) continue;
-            result[index++] = source[i];
-        }
-        return result;
+    private static void reverse(String s) {
+        for (int i = s.length() - 1; i >= 0; i--) System.out.println(s.charAt(i));
     }
 }
