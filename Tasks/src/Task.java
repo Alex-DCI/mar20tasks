b9171ee9e4dd4be1ec7a74642575e7d5c6114b49
Oleksandr <oleksandr.ovcharenko@dci-students.org>
2024-03-20 11:08:29 +0100

    task5 done

--- Tasks/src/Task.java
+++ Tasks/src/Task.java
@@ -1,5 +1,8 @@
 public class Task {
     public static void main(String[] args) {
-        for (int i = 0; i < 9; i++) System.out.print("1 ");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) System.out.print("+ ");
+            System.out.println();
+        }
     }
 }
