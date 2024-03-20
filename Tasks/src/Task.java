4493361935a8b2f0ad240c86e44f94a96bd9aae7
Oleksandr <oleksandr.ovcharenko@dci-students.org>
2024-03-20 13:37:34 +0100

    task13 done

--- Tasks/src/Task.java
+++ Tasks/src/Task.java
@@ -1,10 +1,25 @@
+import java.util.Arrays;
 public class Task {
     public static void main(String[] args) {
-        String[] result = repeatItem("AnyStringYouWantToClone", 8);
+        Integer[] array = {1, 4, 4, 7, 7, 7, 8};
+        System.out.println(Arrays.toString(noDuplicates(array)));
     }
-    private static String[] repeatItem (String source, int count) {
-        String[] result = new String[count];
-        for (int i = 0; i < count; i++) result[i] = source;
+    private static int[] noDuplicates(Integer[] source) {
+        int repeats = 0, index = 0;
+        for (int i = 1; i < source.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (source[j] != null && source[j] == source[i]) {
+                    repeats++;
+                    source[j] = null;
+                    break;
+                }
+            }
+        }
+        int[] result = new int[source.length - repeats];
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == null) continue;
+            result[index++] = source[i];
+        }
         return result;
     }
 }
